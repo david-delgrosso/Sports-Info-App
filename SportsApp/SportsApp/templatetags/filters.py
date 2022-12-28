c173a00859b0431d344304d55d3de454b9cc84ef
david-delgrosso <daviddelgrosso97@gmail.com>
2022-12-27 23:01:32 -0500

    -fix game time display format

--- SportsApp/SportsApp/templatetags/filters.py
+++ SportsApp/SportsApp/templatetags/filters.py
@@ -19,3 +19,12 @@ def addstrings(s1, s2):
 @register.filter(name='calcpercent')
 def calcpercent(n, d):
     return round(float(n/d) * 100, 1)
+
+# Assumes all games are 12:00 PM or later
+@register.filter(name='timeformat')
+def timeformat(value):
+    val_str = str(value)
+    val_str_sp = val_str.split(':')
+    hr = int(val_str_sp[0]) - 12
+    time_str = f"{hr}:{val_str_sp[1]} PM"
+    return time_str
